1c90d80bab2e62ddb695fd9958b15df75755c5e2
Gary Chang <chang324@gmail.com>
2021-02-07 18:52:11 -0500

    add some basic yf for testing

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import yfinance as yf
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, from flask vscode"
+
+
+@app.route("/quote")
+def display_quote():
+    symbol = request.args.get('symbol', default="AAPL")
+
+    quote = yf.Ticker(symbol)
+
+    return quote.info
+
+if __name__ == "__main__":
+    app.run(debug=True)
